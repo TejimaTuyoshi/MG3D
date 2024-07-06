a2a05fdb2fd88e2453451c15466409f0a2f2450d
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-07-06 12:57:55 +0900

    移動の修正+ジャンプの追加

--- MG3D/Assets/Script/Player.cs
+++ MG3D/Assets/Script/Player.cs
@@ -8,21 +8,24 @@ public class Player : MonoBehaviour
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        Time.timeScale = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
         if (Input.GetKey("a") && !isStop)
         {
             transform.position += transform.TransformDirection(Vector3.forward) * 0.5f;
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.rotation = Quaternion.Euler(0, 270, 0);
         }
         if (Input.GetKey("d") && !isStop)
         {
             transform.position += transform.TransformDirection(Vector3.back) * 0.5f;
-            transform.rotation = Quaternion.Euler(0, 180, 0);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
         }
+
         if (Input.GetKey("w") && !isStop)
         {
             transform.position += transform.TransformDirection(Vector3.right) * 0.5f;
@@ -30,24 +33,17 @@ public class Player : MonoBehaviour
         if (Input.GetKey("s") && !isStop)
         {
             transform.position += transform.TransformDirection(Vector3.left) * 0.5f;
-            transform.rotation = Quaternion.Euler(0, 270, 0);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
         }
-    }
-
-    private void FixedUpdate()
-    {
-        if (isStop)
+        if (Input.GetKeyDown("space") && !isStop)
         {
-            Time.timeScale = 0.0f;
-        }
-        else
-        {
-            Time.timeScale = 1.0f;
+            rigidBody.AddForce(Vector3.up * 500, ForceMode.Force);
         }
     }
 
     public void start()
     {
         isStop = false;
+        Time.timeScale = 1.0f;
     }
 }
