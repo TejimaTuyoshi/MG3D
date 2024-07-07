888dbbb2e1fa6b8f36fbecf2035f37e84cc7b454
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-07-07 12:21:25 +0900

    プレイヤーの移動方法の変更

--- MG3D/Assets/Script/Player.cs
+++ MG3D/Assets/Script/Player.cs
@@ -17,27 +17,27 @@ public class Player : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, 0, 0);
         if (Input.GetKey("a") && !isStop)
         {
-            transform.position += transform.TransformDirection(Vector3.forward) * 0.5f;
+            rigidBody.velocity += new Vector3(0, 0, 0.5f);
             transform.rotation = Quaternion.Euler(0, 270, 0);
         }
         if (Input.GetKey("d") && !isStop)
         {
-            transform.position += transform.TransformDirection(Vector3.back) * 0.5f;
+            rigidBody.velocity += new Vector3(0, 0, -0.5f);
             transform.rotation = Quaternion.Euler(0, 90, 0);
         }
 
         if (Input.GetKey("w") && !isStop)
         {
-            transform.position += transform.TransformDirection(Vector3.right) * 0.5f;
+            rigidBody.velocity += new Vector3(0.5f, 0, 0);
         }
         if (Input.GetKey("s") && !isStop)
         {
-            transform.position += transform.TransformDirection(Vector3.left) * 0.5f;
+            rigidBody.velocity += new Vector3(-0.5f, 0, 0);
             transform.rotation = Quaternion.Euler(0, 180, 0);
         }
         if (Input.GetKeyDown("space") && !isStop)
         {
-            rigidBody.AddForce(Vector3.up * 500, ForceMode.Force);
+            rigidBody.velocity += new Vector3(0, 10, 0);
         }
     }
 
