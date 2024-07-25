36c882c95a0d6ac52792f24f7adf00223f015b3d
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-07-25 12:17:58 +0900

    エネミーに仮の移動を追加

--- MG3D/Assets/Script/Enemy.cs
+++ MG3D/Assets/Script/Enemy.cs
@@ -1,21 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Enemy: MonoBehaviour
 {
+    Transform enemyTransform;
+    Player player = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindObjectOfType<Player>();
+        enemyTransform = this.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (this.gameObject.transform.position.y <= 0)
+        if (player.transform.position.x < this.transform.position.x)
+        {
+            enemyTransform.Translate(-0.04f, 0, 0);
+        }
+        else if (player.transform.position.x > this.transform.position.x)
+        {
+            enemyTransform.Translate(0.04f, 0, 0);
+        }
+
+        if (player.transform.position.z < this.transform.position.z)
+        {
+            enemyTransform.Translate(0, 0, -0.04f);
+        }
+        else if (player.transform.position.z > this.transform.position.z)
+        {
+            enemyTransform.Translate(0, 0, 0.04f);
+        }
+
+            if (this.gameObject.transform.position.y <= 0)
         {
             transform.position += new Vector3(0,10,0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hit"))
+        {
+            this.gameObject.SetActive(false);
         }
     }
 }
