823863789b2e0dc7ba6078ac2f15497b90dfc59b
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-08-21 14:37:25 +0900

    ジェネレーターの仕様の変更

--- MG3D/Assets/Script/Generator.cs
+++ MG3D/Assets/Script/Generator.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 
+
+public enum InstanceEnemys
+{
+    Simple,
+    Quick,
+    shooter
+}
 public class Generator : MonoBehaviour
 {
     [SerializeField]float time = 0f;
     [SerializeField] EnemyCount enemycount;
     [SerializeField]GameObject obj;
+    [SerializeField] GameObject quickObj;
+    [SerializeField] GameObject shooterObj;
+    [SerializeField] InstanceEnemys enemys;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +30,9 @@ public class Generator : MonoBehaviour
         {
             time = 0;
             enemycount.Plus();
-            Instantiate(obj, new Vector3(gameObject.transform.position.x, 0.4f, gameObject.transform.position.z), Quaternion.identity);
+            if (enemys == InstanceEnemys.Simple){Instantiate(obj, new Vector3(gameObject.transform.position.x, 0.4f, gameObject.transform.position.z), Quaternion.identity);}
+            if (enemys == InstanceEnemys.Quick) { Instantiate(quickObj, new Vector3(gameObject.transform.position.x, 0.4f, gameObject.transform.position.z), Quaternion.identity); }
+            if (enemys == InstanceEnemys.shooter) { Instantiate(shooterObj, new Vector3(gameObject.transform.position.x, 0.4f, gameObject.transform.position.z), Quaternion.identity); }
         }
     }
 }
