023c1919faf2364308987f60da2770301399b7d9
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-08-26 09:30:01 +0900

    遠距離からの攻撃する敵の投げ方を修正

--- MG3D/Assets/Script/ShooterEnemy.cs
+++ MG3D/Assets/Script/ShooterEnemy.cs
@@ -3,65 +3,53 @@ using UnityEngine;
 public class ShooterEnemy : MonoBehaviour
 {
     [SerializeField] GameObject ammo;
-    [SerializeField] GameObject enemy;
-    [SerializeField] Collider enemyArea;
-
-    float coolTime = 0f;
-    [SerializeField] float shotlimit = 0f;
-    [SerializeField] float limit = 3f;
-    [SerializeField] float ammolimit = 1f;
-    [SerializeField] bool isjump = false;
-    [SerializeField] bool isshot = false;
-    [SerializeField] bool islimit = false;
+    [SerializeField] float x = 0f;
+    [SerializeField] float y = 0f;
+    [SerializeField] float z = 0f;
+    [SerializeField] float playerX = 0f;
+    [SerializeField] float playerY = 0f;
+    [SerializeField] float playerZ = 0f;
+    [SerializeField] float distanceX = 0f;
+    [SerializeField] float distanceY = 0f;
+    [SerializeField] float distanceZ = 0f;
+    [SerializeField] float limitX = 5f;
+    [SerializeField] float limitY = 2f;
+    [SerializeField] float limitZ = 5f;
+    float time = 0f;
+    Player player;
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindObjectOfType<Player>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        coolTime += Time.deltaTime;
-        shotlimit += Time.deltaTime;
-        if (coolTime < limit)
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
+        playerX = player.transform.position.x;
+        playerY = player.transform.position.y;
+        playerZ = player.transform.position.z;
+        distanceX = playerX - x;
+        distanceY = playerY - y;
+        distanceZ = playerZ - z;
+        if (distanceX < 0) { distanceX *= -1; }
+        if (distanceY < 0) { distanceY *= -1; }
+        if (distanceZ < 0) { distanceZ *= -1; }
         {
-            isjump = true;
-            coolTime = 0f;
-        }
-        else
-        {
-            enemyArea.enabled = false;
-            isjump = false;
-        }
 
-        if (shotlimit >= ammolimit)
-        {
-            islimit = true;
-            shotlimit = 0f;
         }
-        else
+        if (distanceX < limitX && distanceY < limitY && distanceZ < limitZ)
         {
-            islimit = false;
-        }
-
-        if (isjump)
-        {
-            enemyArea.enabled = true;
-            enemy.transform.position += transform.TransformDirection(Vector3.up) * 0.1f;
-        }
-
-        if (isshot && islimit)
-        {
-            Instantiate(ammo, new Vector3(gameObject.transform.position.x + 0.1f, gameObject.transform.position.y + 1f, gameObject.transform.position.z), Quaternion.identity);
+            time += Time.deltaTime;
+            if (time >= 1)
+            {
+                time = 0;
+                Instantiate(ammo, new Vector3(x, y + 1f, z), Quaternion.identity);
+            }
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            isshot = true;
-        }
-    }
 }
