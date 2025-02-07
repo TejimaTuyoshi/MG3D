f7c015ed8ee97a5d34b95b5a23f8ad398059000a
LIFE4649\tejim <tejima.tuyoshi@icloud.com>
2025-02-07 15:57:59 +0900

    フラグで出現する足場を作成

---
+++ MG3D/Assets/Script/MovingRoute.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+
+public class MovingRoute : MonoBehaviour
+{
+    [SerializeField] GameObject route;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            route.SetActive(true);
+        }
+    }
+}
